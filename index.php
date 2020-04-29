31842c7d122b807fd22512d05a51923a1ba9f025
tabr <tabr@xaker.ru>
2020-04-29 23:49:13 +0300

    gitignore & trash removed.

--- index.php
+++ index.php
@@ -18,39 +18,3 @@ $C = &$_SESSION['C'];
 //TODO: add checks that there is our class, but not now
 $C->Polling();
 $C->EndOfPage();
-?>
-<section class="a">
-    <div class="container">
-        <div class="row">
-            <div class="a1 col-lg-6">A1</div>
-            <div class="a2 col-lg-6">A2</div>
-        </div>
-    </div>
-</section>
-
-<section class="b">
-    <div class="container">
-        B
-    </div>
-</section>
-
-<section class="c">
-    <div class="container">
-        <div class="row">
-            <div class="c1 col-sm-6 col-lg-4">C1</div>
-            <div class="c2 col-sm-6 col-lg-4">C2</div>
-            <div class="c3 col-lg-4">C3</div>
-        </div>
-    </div>
-</section>
-
-<section class="d">
-    <div class="container">
-        <div class="row">
-            <div class="d1 col-sm-6 col-lg-3">D1</div>
-            <div class="d2 col-sm-6 col-lg-3">D2</div>
-            <div class="d3 col-sm-6 col-lg-3">D3</div>
-            <div class="d4 col-sm-6 col-lg-3">D4</div>
-        </div>
-    </div>
-</section>
